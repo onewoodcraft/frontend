52663f150a50f6c87c88d9d8573df012f3420cee
Yogi Faujdar <apple@MacBook-Pro.local>
2025-05-17 14:18:58 +0530

    fix: add client directive and fix color prop in loader component

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,7 +1,8 @@
+'use client';
 import React from "react";
 import { FadeLoader, BarLoader } from "react-spinners";
 
-const Loader = ({ loading, spinner = "scale",color="0989FF" }) => {
+const Loader = ({ loading, spinner = "scale", color = "0989FF" }) => {
   return (
     <div className="text-center">
       {spinner === "scale" && (
@@ -13,7 +14,7 @@ const Loader = ({ loading, spinner = "scale",color="0989FF" }) => {
           margin={2}
         />
       )}
-      {spinner === "fade" && <FadeLoader loading={loading} color="#0989FF" />}
+      {spinner === "fade" && <FadeLoader loading={loading} color={`#${color}`} />}
     </div>
   );
 };
