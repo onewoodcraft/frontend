bf14e75c53a4510da0a160fe84e3b10ae5a31f31
Yogi Faujdar <apple@MacBook-Pro.local>
2025-05-17 14:15:10 +0530

    refactor: restructure loader index to use single export pattern

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,46 +1,58 @@
+// Home loaders
 import HomeCateLoader from "./home/home-cate-loader";
 import HomeGadgetPrdLoader from "./home/home-gadget-prd-loader";
 import HomeNewArrivalPrdLoader from "./home/home-newArrival-prd-loader";
 import HomeOfferPrdLoader from "./home/home-offer-prd-loader";
 import HomePrdLoader from "./home/home-prd-loader";
 import HomeSmPrdLoader from "./home/home-sm-prd-loader";
+
+// Home one loaders
+import HomeOnePrdLoader from './home-1/home-1-prd-loader';
+import HomeOneCatLoader from './home-1/home-1-cat-loader';
+import HomeOneFeatureLoader from './home-1/home-1-feature-loader';
+
+// Home two loaders
 import HomeTwoCateLoader from "./home-2/home-2-cate-loader";
 import HomeTwoFeaturedPrdLoader from "./home-2/home-2-featured-loader";
 import HomeTwoPopularPrdLoader from "./home-2/home-2-popular-loader";
 import HomeTwoPrdLoader from "./home-2/home-2-prd-loader";
 import HomeTwoNewPrdPrdLoader from "./home-2/home-2-newPrd-loader";
 import HomeTwoBestSellPrdPrdLoader from "./home-2/home-2-bestSell-loader";
-import HomeThreeCategoryLoader from "./home-3/beauty-category-loader";
-import HomeThreePrdLoader from "./home-3/beauty-prd-loader";
-import HomeThreeTrendingPrdLoader from "./home-3/beauty-trendingPrd-loader";
-import HomeThreePrdTwoLoader from "./home-3/beauty-prd-2-loader";
 
-// home one loaders
-export { default as HomeOnePrdLoader } from './home-1/home-1-prd-loader';
-export { default as HomeOneCatLoader } from './home-1/home-1-cat-loader';
-export { default as HomeOneFeatureLoader } from './home-1/home-1-feature-loader';
+// Shop loaders
+import ShopCatLoader from './shop/shop-cat-loader';
+import ShopLoader from './shop/shop-loader';
+import ShopDetailsLoader from './shop/shop-details-loader';
+import PrdDetailsLoader from './prd-details-loader';
+import SearchPrdLoader from './search-prd-loader';
 
-// home two loaders
-export { default as HomeTwoPrdLoader } from './home-2/home-2-prd-loader';
-export { default as HomeTwoFeaturedPrdLoader } from './home-2/home-2-featured-loader';
-export { default as HomeTwoNewPrdPrdLoader } from './home-2/home-2-newPrd-loader';
-export { default as HomeTwoBestSellPrdPrdLoader } from './home-2/home-2-bestSell-loader';
-export { default as HomeTwoPopularPrdLoader } from './home-2/home-2-popular-loader';
-
-// shop loader 
-export { default as ShopCatLoader } from './shop/shop-cat-loader';
-export { default as ShopLoader } from './shop/shop-loader';
-export { default as ShopDetailsLoader } from './shop/shop-details-loader';
-export { default as PrdDetailsLoader } from './prd-details-loader';
-export { default as SearchPrdLoader } from './search-prd-loader';
-
+// Export all loaders
 export {
+  // Home loaders
   HomeCateLoader,
   HomeGadgetPrdLoader,
   HomeNewArrivalPrdLoader,
   HomeOfferPrdLoader,
   HomePrdLoader,
   HomeSmPrdLoader,
+  
+  // Home one loaders
+  HomeOnePrdLoader,
+  HomeOneCatLoader,
+  HomeOneFeatureLoader,
+  
+  // Home two loaders
   HomeTwoCateLoader,
-  HomeTwoBestSellPrdPrdLoader
-}
+  HomeTwoFeaturedPrdLoader,
+  HomeTwoPopularPrdLoader,
+  HomeTwoPrdLoader,
+  HomeTwoNewPrdPrdLoader,
+  HomeTwoBestSellPrdPrdLoader,
+  
+  // Shop loaders
+  ShopCatLoader,
+  ShopLoader,
+  ShopDetailsLoader,
+  PrdDetailsLoader,
+  SearchPrdLoader
+};
